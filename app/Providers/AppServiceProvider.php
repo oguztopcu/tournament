214472b1af06f46e75495b489b5172929bc43b3d
oguztopcu <oguztopcu7@gmail.com>
2024-01-19 04:23:53 +0300

    Route bindings for model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Tournament;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->routeBindings();
+    }
+
+    protected function routeBindings(): void
+    {
+        Route::model('tournamentId', Tournament::class);
     }
 }
